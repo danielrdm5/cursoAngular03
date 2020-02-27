afa829e4d7051ec60e975dbb02664def975fdd18
danielrdm5 <daniel_rdm@hotmail.com>
2020-02-26 17:10:23 -0800

    Avance clase 80 Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Capitán América';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  personajes: string[] =['Goku', 'Iron Man', 'Vegeta', 'El Chavo'];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 12345.2;
+
+  heroe = {
+    nombre: 'El Chavo',
+    edad: '678',
+    direccion:{
+      calle: 'Primera',
+      casa: 'Barril',
+      lugar: 'La vecindad'
+    }
+  };
 }
